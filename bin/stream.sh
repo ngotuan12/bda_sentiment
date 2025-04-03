cf7b1ccbd1fc1195efc1f2c920c0dd387888be5e
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2025-04-03 16:57:14 +0700

    up

---
+++ bin/stream.sh
@@ -0,0 +1,1 @@
+spark-submit --master yarn --deploy-mode client src/stream.py --jars /home/hdfs/jar/spark-sql-kafka-0-10_2.12-3.5.5.jar,/home/hdfs/kafka/kafka_2.13-4.0.0/libs/kafka-clients-4.0.0.jar
