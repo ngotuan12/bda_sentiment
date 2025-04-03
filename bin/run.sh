b86d485562fbea5c85388b32798a320b97a502ae
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2025-04-03 13:13:25 +0700

    up

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Chạy ứng dụng Spark
 spark-submit --master yarn --deploy-mode client /home/hdfs/study/bda_sentiment/src/app.py
+
+# Lấy thư mục model mới nhất (dựa vào timestamp)
+LATEST_MODEL=$(hdfs dfs -ls /study/sentiment/model | grep -v Found | sort -k6,7 -r | head -n 1 | awk '{print $8}')
+echo "Thư mục model mới nhất: $LATEST_MODEL"
+
+# Gộp các file metrics
+hdfs dfs -getmerge ${LATEST_MODEL}/metrics/* /tmp/merged_metrics.csv
+
+# Upload lại file đã gộp
+hdfs dfs -put -f /tmp/merged_metrics.csv ${LATEST_MODEL}/metrics_combined.csv
+
+echo "Đã gộp metrics thành công: ${LATEST_MODEL}/metrics_combined.csv"
