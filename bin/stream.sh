4e9c83d16599e0d7053a5531f38821c3352f83e0
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2025-04-04 09:36:19 +0700

    up

--- bin/stream.sh
+++ bin/stream.sh
@@ -1,2 +1,3 @@
-JAVA_HOME=/home/hdfs/jdk/jdk-17.0.14/  spark-submit --master yarn --deploy-mode client src/stream.py \
+JAVA_HOME=/home/hdfs/jdk/jdk-17.0.14/  spark-submit --master yarn \
+  --deploy-mode client src/stream.py \
   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.5
